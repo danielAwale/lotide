98cc3c651ae215266411d1b631984f9193bdcabb
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-06-11 20:44:30 +0000

    eqArrays donegit add .

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🔥🔥🔥Assertion Failed: ${actual} !== ${expected}`);
-  }
+const assertEqual = require('./assertEqual');
 
-};
 
 const eqArrays = function(arr1, arr2){
   if(arr1.length !== arr2.length){
@@ -18,8 +12,4 @@ const eqArrays = function(arr1, arr2){
     return true;
   }
 
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2"]));
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+module.exports = eqArrays;
