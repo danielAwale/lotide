b1ebdbf00b2760ab547e5acdcb2b30bd2a6ca33e
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-06-11 21:57:29 +0000

    refactered assertarraysequal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,14 @@
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2){
-  for (let i = 0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i] && arr1.length !== arr2.length)
+  if(!eqArrays(arr1,arr2)){
     console.log(`🔥🔥🔥Assertion Failed: ${arr1} !== ${arr2}`);
-    }
+    } else {
     console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+    }
   }
+  
 
 assertArraysEqual([1,2,3,4,5],[1,2,3,4,5]);
+
+module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -1,22 +1,13 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
+const eqArrays = function(arr1, arr2){
+  if(arr1.length !== arr2.length){
     return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i])
-      return false;
-  }
-  return true;
-};
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🔥🔥🔥Assertion Failed: ${actual} !== ${expected}`);
+  } 
+  for (let i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i])
+        return false;
+    }
   }
 
-};
 
 const middle = function(array) {
   let result = Math.floor(array.length / 2);
@@ -29,7 +20,9 @@ const middle = function(array) {
   return array[result];
 };
 
-console.log(middle([1,1,1,2,2,2]));
-console.log(middle([1,2,3,4,5]));
-console.log(middle([1,2]));
-console.log(middle([0]));
+equalArrays((middle([1,1,1,2,2,2])));
+equalArrays((middle([1,2,3,4,5])));
+equalArrays((middle([1,2])));
+equalArrays((middle([0])));
+
+module.exports = middle;
