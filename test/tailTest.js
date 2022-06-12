56c871e8a261e4a7b309079e838f6607227f5836
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-06-12 02:54:27 +0000

    testing tail with mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,11 @@
+const { assert } = require('chai');
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
+describe("#tail", () => {
+  it("returns [2,3,4,5,6,7,8,9] for [1,2,3,4,5,6,7,8,9]", () => {
+    assert.deepEqual(tail([1,2,3,4,5,6,7,8,9]), [2,3,4,5,6,7,8,9])
+  });
+});
+
  
-const words = [1,2,3,4,5,6,7,8,9];
-tail(words);
-assertEqual(words.length, 9);
